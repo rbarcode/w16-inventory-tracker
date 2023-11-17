6d8bcd6430d386800282dee7798e24ab945c9a59
Richard Barbour <richyb07@gmail.com>
2023-11-17 09:21:16 -0800

    Add Tea.js component.

---
+++ src/components/Tea.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Tea(props){
+  return (
+    <React.Fragment>
+      <h3>{props.name}</h3>
+      <h5>Origin: {props.origin}</h5>
+      <h5>Type: {props.type}</h5>
+      <h5>Price: {props.price}</h5>
+      <h5>Remaining Inventory: {props.quantity}oz.</h5>
+
+    </React.Fragment>
+  )
+};
+
+export default Tea;
